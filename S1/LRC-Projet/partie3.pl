2772a1415c62042f4f4546ea1bfec39d0df1bafa
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2023-05-29 22:05:22 +0200

    Correction de la partie 3

--- S1/LRC-Projet/partie3.pl
+++ S1/LRC-Projet/partie3.pl
@@ -16,13 +16,12 @@ troisieme_etape(Abi,Abr) :-	tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),
 
 
 % Tri de la A_Box étendue en fonction de la relation considérée
-tri_Abox([], Lie, Lpt, Li, Lu, Ls).
+tri_Abox([], [], [], [], [], []).
 tri_Abox([ (I,some(R,C)) | Abi], [ (I,some(R,C)) | Lie], Lpt, Li, Lu, Ls) :-	tri_Abox(Abi, Lie, Lpt, Li, Lu, Ls), !.
 tri_Abox([ (I,all(R,C)) | Abi], Lie, [ (I,all(R,C)) | Lpt], Li, Lu, Ls) :-		tri_Abox(Abi, Lie, Lpt, Li, Lu, Ls), !.
 tri_Abox([ (I,and(C1,C2)) | Abi], Lie, Lpt, [ (I,and(C1,C2)) | Li], Lu, Ls) :-	tri_Abox(Abi, Lie, Lpt, Li, Lu, Ls), !.
 tri_Abox([ (I,or(C1,C2)) | Abi], Lie, Lpt, Li, [ (I,or(C1,C2)) | Lu], Ls) :-	tri_Abox(Abi, Lie, Lpt, Li, Lu, Ls), !.
 tri_Abox([ (I,C) | Abi], Lie, Lpt, Li, Lu, [ (I,C) | Ls]) :-					tri_Abox(Abi, Lie, Lpt, Li, Lu, Ls), !.
-tri_Abox([ (I,not(C)) | Abi], Lie, Lpt, Li, Lu, [ (I,not(C)) | Ls]) :-			tri_Abox(Abi, Lie, Lpt, Li, Lu, Ls), !.
 
 
 
@@ -35,6 +34,7 @@ resolution([], Lpt, Li, Lu, Ls, Abr) :-	pas_clash(Ls), transformation_and([], Lp
 resolution([], Lpt, [], Lu, Ls, Abr) :-	pas_clash(Ls), deduction_all([], Lpt, [], Lu, Ls, Abr), !.
 resolution([], [], [], Lu, Ls, Abr):-	pas_clash(Ls), transformation_or([], [], [], Lu, Ls, Abr), !.
 resolution([], [], [], [], Ls, Abr):-	pas_clash(Ls), !.
+% Attention à la dernière clause
 
 
 % Vérifie la présence d'un clash
